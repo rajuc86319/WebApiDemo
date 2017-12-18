dd7f17afa2d2ce9489ce4a36469309fbd51d345c
rajuc86319 <34654289+rajuc86319@users.noreply.github.com>
2017-12-18 21:27:51 +0530

    Add files via upload

---
+++ DemoWepApi/DemoWepApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace DemoWepApi
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+			
+			
+			// Web API routes
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "Employee",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { controller = "Employee", action = "Get", id = RouteParameter.Optional }
+			);
+			config.Formatters.Remove(config.Formatters.XmlFormatter);
+			config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+			config.Filters.Add(new BasicAuthenticationAttribute());
+		}
+	}
+}
